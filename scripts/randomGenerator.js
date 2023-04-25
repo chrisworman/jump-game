7f0362fb4e1499faffb4c1547760faec0a36ae1c
Chris Worman <chrisworman@gmail.com>
2023-04-25 16:36:13 -0700

    Prettier

--- scripts/randomGenerator.js
+++ scripts/randomGenerator.js
@@ -5,17 +5,17 @@ export class RandomGenerator {
             y: RandomGenerator.randomIntBetween(0, canvasHeight - itemHeight),
         };
     }
-    
+
     static randomIntBetween(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    
+
     static randomFromArray(array) {
         return array[RandomGenerator.randomIntBetween(0, array.length - 1)];
     }
-    
+
     static randomSign() {
         return RandomGenerator.randomIntBetween(0, 1) === 0 ? -1 : 1;
     }
