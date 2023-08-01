ed569aafb210926694a72c16fd602a7361b7c95e
Chris Worman <chrisworman@gmail.com>
2023-08-01 15:41:06 -0700

    Switch from mp3 to aac for audio format

--- src/scripts/audioManager.js
+++ src/scripts/audioManager.js
@@ -1,96 +1,96 @@
 export class AudioManager {
     static AUDIO_FILES = {
         BACKGROUND_SONG: {
-            url: 'sounds/piano-arp-live-update.mp3',
+            url: 'sounds/piano-arp-live-update.aac',
             volume: 0.07,
         },
         BOSS_SONG: {
-            url: 'sounds/desert-trail.mp3',
+            url: 'sounds/desert-trail.aac',
             volume: 0.35,
         },
         FINALE_SONG: {
-            url: 'sounds/stranger-mix-2.mp3',
+            url: 'sounds/stranger-mix-2.aac',
             volume: 0.2,
         },
         BOSS_CELEBRATION_SONG: {
-            url: 'sounds/boss-celebration.mp3',
+            url: 'sounds/boss-celebration.aac',
             volume: 0.3,
         },
         SHIELD_SONG: {
-            url: 'sounds/shield-song.mp3',
+            url: 'sounds/shield-song.aac',
             volume: 0.4,
         },
         GEM_COLLECTED: {
-            url: 'sounds/collectable.mp3',
+            url: 'sounds/collectable.aac',
             volume: 0.6,
         },
         BIG_BOMB: {
-            url: 'sounds/fire-ball.mp3',
+            url: 'sounds/fire-ball.aac',
             volume: 0.025,
         },
         HEALTH_UP: {
-            url: 'sounds/health-up.mp3',
+            url: 'sounds/health-up.aac',
             volume: 0.2,
         },
         PLAYER_JUMP: {
-            url: 'sounds/jump.mp3',
+            url: 'sounds/jump.aac',
             volume: 0.08,
         },
         PLAYER_SHOOT: {
-            url: 'sounds/shoot.mp3',
+            url: 'sounds/shoot.aac',
             volume: 0.16,
         },
         LASER_COLLECTED: {
-            url: 'sounds/laser-collected.mp3',
+            url: 'sounds/laser-collected.aac',
             volume: 0.08,
         },
         LASER_GUN: {
-            url: 'sounds/laser-gun.mp3',
+            url: 'sounds/laser-gun.aac',
             volume: 0.25,
         },
         BOMB: {
-            url: 'sounds/bomb.mp3',
+            url: 'sounds/bomb.aac',
             volume: 0.04,
         },
         PLAYER_HIT: {
-            url: 'sounds/player-hit.mp3',
+            url: 'sounds/player-hit.aac',
             volume: 0.35,
         },
         ENEMY_HIT: {
-            url: 'sounds/enemy-hit.mp3',
+            url: 'sounds/enemy-hit.aac',
             volume: 0.3,
         },
         START_BOSS_LEVEL: {
-            url: 'sounds/start-boss-level.mp3',
+            url: 'sounds/start-boss-level.aac',
             volume: 0.18,
         },
         ROBOTIC_DEATH: {
             // Unused, but neat, use for final boss!!
-            url: 'sounds/robotic-death.mp3',
+            url: 'sounds/robotic-death.aac',
             volume: 0.05,
         },
         BOSS_SHOT_1: {
-            url: 'sounds/boss-hit.mp3',
+            url: 'sounds/boss-hit.aac',
             volume: 0.3,
         },
         BOSS_SHOT_2: {
-            url: 'sounds/boss-hit-2.mp3',
+            url: 'sounds/boss-hit-2.aac',
             volume: 0.3,
         },
         BOSS_SHOT_3: {
-            url: 'sounds/boss-hit-3.mp3',
+            url: 'sounds/boss-hit-3.aac',
             volume: 0.3,
         },
         BOSS_3_SHOT_1: {
-            url: 'sounds/boss-3-vox-1.mp3',
+            url: 'sounds/boss-3-vox-1.aac',
             volume: 0.2,
         },
         BOSS_3_SHOT_2: {
-            url: 'sounds/boss-3-vox-2.mp3',
+            url: 'sounds/boss-3-vox-2.aac',
             volume: 0.2,
         },
         BOSS_DEAD: {
-            url: 'sounds/boss-dead.mp3',
+            url: 'sounds/boss-dead.aac',
             volume: 0.25,
         },
     };
